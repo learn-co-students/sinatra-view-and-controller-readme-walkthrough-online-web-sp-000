1606f10ad39bf856cc07aabfbbe70b3489fa0b45
dfenjves <fenjves.daniel@gmail.com>
2015-09-09 14:15:52 -0400

    first draft of lesson

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
-    erb :index
+  get '/reverse' do
+    erb :reverse
+  end
+
+  post '/reverse' do
+    erb :reversed
+  end
+
+  get '/friends' do
+    #Write your code here!
   end
 
 end
