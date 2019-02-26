71c07dbb11ba7ad79099c5d5dccaba575b27b457
jonmackeyparty <jon@jonmackey.party>
2019-02-26 16:55:31 -0500

    complete reverse route, add friends route & iterate through friends list

--- app.rb
+++ app.rb
@@ -6,10 +6,14 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+     original_string = params[:string]
+     @reversed_string = original_string.reverse
     erb :reversed
   end
 
   get '/friends' do
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    erb :friends
     # Write your code here!
 
   end
