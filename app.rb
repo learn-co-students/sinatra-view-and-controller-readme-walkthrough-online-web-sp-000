2e70ccb6e78b39d8499c332e7391ee098a509742
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-27 18:15:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,21 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/reverse' do
     erb :reverse
   end
 
+#Instance variables allow us to bypass scope between the various methods in a class. Creating an instance variable in a controller method (route) lets the contents become 'visible' to the erb file to which it renders.
   post '/reverse' do
   original_string = params["string"]
   @reversed_string = original_string.reverse
-
   erb :reversed
 end
 
     # Write your code here!
     get '/friends' do
       @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
-
       erb :friends
     end
 
